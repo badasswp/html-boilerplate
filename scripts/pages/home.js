89781c700c661e3390f9e42944ebb1e978c555bd
badasswp <badasswpdev@gmail.com>
2025-03-19 09:37:52 +0100

    chore: home page script

---
+++ scripts/pages/home.js
@@ -0,0 +1,9 @@
+/**
+ * DOM Scripts for the Home Page.
+ */
+(() => {
+	document.addEventListener('DOMContentLoaded', function () {
+		// JavaScript code goes here
+		console.log('Home Page');
+	});
+})();
